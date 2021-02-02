2eeb63c5b53a4e2db6730ce69a885b20b0ef56df
echoshihab <echoshihab@gmail.com>
2021-02-01 19:30:56 -0500

    add PhoneWord App

---
+++ PhoneWord/PhoneWord/App.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace PhoneWord
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+
+            var button = new Button
+            {
+                Text = "Hello"
+            };
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
